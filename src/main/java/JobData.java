a43ca5700fa415d236c4954d8be916761a4ba044
Iurii Rudov <iuriirudov@gmail.com>
2022-01-27 23:31:05 -0500

    Created method findByValue

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -99,7 +99,15 @@ public class JobData {
         loadData();
 
         // TODO - implement this method
-        return null;
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> row : allJobs) {
+            if (row.containsValue(value)) {
+                jobs.add(row);
+            }
+        }
+
+        return jobs;
     }
 
     /**
